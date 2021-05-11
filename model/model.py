4abcb830134e109d21dcf6b0a8e26a797821ea84
shantanukaushik21 <shantanu.kaushik21@gmail.com>
2021-05-12 00:49:18 +0530

    Hindi HandWriting Recognition

---
+++ model/model.py
@@ -0,0 +1,69 @@
+
+import numpy as np
+import pandas as pd
+import tensorflow.keras.backend as K
+
+from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization, Flatten, Conv2D, SeparableConv2D, Concatenate
+from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam, SGD
+
+
+def vanilla_conv(image_x,image_y):
+    num_of_classes = 37
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(5, 5), input_shape=(image_x, image_y, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(5, 5), strides=(5, 5), padding='same'))
+    model.add(Flatten())
+    model.add(Dense(num_of_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    
+    return model
+
+def sc_conv(image_x,image_y):
+    num_of_classes = 37
+    inp_ = Input((image_x, image_y, 1)) # Greyscale Image
+    x = SeparableConv2D(filters = 64, kernel_size = (3, 3), activation='relu', padding = 'same')(inp_)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    x = SeparableConv2D(filters = 32, kernel_size = (3, 3), activation='relu', padding = 'same')(x)
+    x = MaxPooling2D(pool_size=(5, 5), strides=(5, 5), padding='same')(x)
+    x = Flatten()(x)
+    x = Dense(1024, activation = "relu")(x)
+    x = Dropout(0.25)(x)    
+    out = Dense(num_of_classes, activation='softmax')(x)
+    
+    model = Model(inp_, out)
+    
+    model.compile(loss='categorical_crossentropy', optimizer = Adam(lr = 1e-3), metrics=['accuracy'])
+    
+    model.summary()
+
+    return model
+
+def sc_conv_complex(image_x,image_y):
+    num_of_classes = 37
+    inp_ = Input((image_x, image_y, 1)) # Greyscale Image
+    a = SeparableConv2D(filters = 64, kernel_size = (3, 3), activation='relu', padding = 'same')(inp_)
+    a = BatchNormalization()(a)
+    a = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(a)
+    b = SeparableConv2D(filters = 128, kernel_size = (5, 5), activation='relu', padding = 'same')(inp_)
+    b = BatchNormalization()(b)
+    b = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(b)
+    x = Concatenate()([a, b])
+    d = GlobalAveragePooling2D()(x)
+    e = GlobalMaxPooling2D()(x)
+    y = Concatenate()([d, e])
+    y = Dense(1024, activation = "relu")(y)
+    y = Dropout(0.25)(y)
+    out = Dense(num_of_classes, activation='softmax')(y)
+    
+    model = Model(inp_, out)
+    
+    model.compile(loss='categorical_crossentropy', optimizer = Adam(lr = 1e-3), metrics=['accuracy'])
+
+    model.summary()
+    
+    return model
---
+++ model/train.py
@@ -0,0 +1,48 @@
+
+import pandas as pd
+import numpy as np
+
+from tensorflow.keras.utils import to_categorical
+from model import *
+
+
+data = pd.read_csv("data.csv")
+dataset = np.array(data)
+np.random.shuffle(dataset)
+X = dataset
+Y = dataset
+X = X[:, 0:1024]
+Y = Y[:, 1024]
+
+image_x = 32
+image_y = 32
+
+Y = to_categorical(Y)
+X = np.reshape(X, (X.shape[0], image_x, image_y, 1))
+
+# Only uncomment if you want to retrain
+
+# Vanilla model has optimal speed whereas separable conv model has optimal accuracy
+# Introducing additional complexity causes mode collapse
+
+"""
+model_new = vanilla_conv(image_x, image_y)
+
+model_new.fit(X, Y, batch_size = 32, epochs = 10, validation_split =  0.25)
+
+model_new.save("vanilla_conv.h5")
+
+
+model_sc = sc_conv(image_x, image_y)
+
+model_sc.fit(X, Y, batch_size = 32, epochs = 10, validation_split =  0.25)
+
+model_sc.save("sc_conv.h5")
+
+
+model_complex = sc_conv_complex(image_x, image_y)
+
+model_complex.fit(X, Y, batch_size = 32, epochs = 10, validation_split =  0.25)
+
+model_complex.save("sc_conv_complex.h5")
+"""
